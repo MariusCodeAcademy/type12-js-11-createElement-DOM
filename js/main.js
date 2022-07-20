9ac705d1285bd166f1d491503f4a5cee2bf8d08e
Marius <marius@example.com>
2022-07-20 13:57:55 +0300

    append

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict';
 console.log('main.js');
+
+const divEl = document.createElement('div');
+
+divEl.className = 'alert';
+
+/// siukuriam <strong></strong>
+const strongEl = document.createElement('strong');
+strongEl.textContent = 'Hi there';
+
+// divEl.appendChild(strongEl, ' This is and alert'); // prideda tik viena el
+divEl.append(strongEl, ' This is and alert');
+
+// divEl.textContent = ' This is and alert';
+
+document.body.appendChild(divEl);
